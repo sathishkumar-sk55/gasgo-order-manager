bf8936830bfca34a61477fe715fe06892caa09ce
sathish kumar <sathishkumar.sk.0105@gmail.com>
2024-03-05 09:10:53 +0530

    added docker

--- src/main/java/com/frost/gasgo/ordermanager/config/RestTemplateBean.java
+++ src/main/java/com/frost/gasgo/ordermanager/config/RestTemplateBean.java
@@ -1,8 +1,6 @@
 package com.frost.gasgo.ordermanager.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
